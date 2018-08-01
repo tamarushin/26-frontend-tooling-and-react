0f0fad02a5130a76d67d012e0443a894fcd68e91
tamarushin <tamarushin@gmail.com>
2018-07-31 17:21:19 -0700

    Making animals speak and using fragment

--- src/main.js
+++ src/main.js
@@ -1,64 +1,76 @@
-import React from 'react';
+import React, {Component, Fragment} from 'react';
 import ReactDom from 'react-dom';
-
+import faker from 'faker';
+import {DOGE, KISS, KOALA, say} from 'cowsay';
 import './style/app.scss';
 
-class Header extends React.Component {
 
-  render() {
-    return (
-      <header>
-        <h1>Our Counter</h1>
-      </header>
-    );
-  }
-
-}
-
-class App extends React.Component {
+class App extends Component {
 
   constructor(props) {
     super(props);
 
-    this.handleDown = this.handleDown.bind(this);
-    this.handleUp = this.handleUp.bind(this);
-    this.updateState = this.updateState.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.updateAnimal = this.updateAnimal.bind(this);
 
     this.state = {
-      counter: 0,
-      polarity: 'neutral',
+      content: "",
+      cow: "DOGE"
     };
   }
 
-
-  handleUp(e) {
-    this.updateState(this.state.counter + 1);
+  handleClick() {
+    this.setState({
+      content: faker.hacker.phrase()
+    });
   }
 
-  handleDown(e) {
-    this.updateState(this.state.counter - 1);
-  }
+  updateAnimal(event) {
+    if (event.target.value === 'giraffe') {
+      this.setState({cow: GIRAFFE})
+    }
 
-  updateState(counter) {
-    let polarity = null;
-    if (counter > 0) { polarity = 'positive'; }
-    if (counter < 0) { polarity = 'negative'; }
-    this.setState({ counter, polarity });
   }
 
   render() {
     return (
-      <div>
-        <Header />
-        <div id="counterWrapper">
-          <div id="counter" className={this.state.polarity}>{this.state.counter}</div>
-          <a href="#" onClick={this.handleUp} id="up">U</a>
-          <a href="#" onClick={this.handleDown} id="down">D</a>
-        </div>
-      </div>
+      <Fragment>
+        <header>
+//         <h1>My </h1>
+//       </header>
+      
+      </Fragment>
     );
   }
 
 }
 
 ReactDom.render(<App />, document.getElementById('root'));
+newAnimal(event) {
+  if (event.target.value === 'doge') {
+    this.setState({ cow: DOGE });
+  }
+  if (event.target.value === 'kiss') {
+    this.setState({ cow: KISS });
+  }
+  if (event.target.value === 'koala') {
+    this.setState({ cow: KOALA });
+  };
+
+};
+
+render() {
+  return (
+    <Fragment>
+      <h1>My Animals and a kiss</h1>
+      <button onClick={this.handleClick}>Push my BUTTON!</button>
+      <select onChange={this.newAnimal}>
+        <option value="doge">Get along little doge</option>
+        <option value="kiss">K-I-S-S-I-N-G</option>
+        <option value="koala">BEAR!</option>
+      
+      </select>
+      <pre>{say({ text: this.state.content, cow: this.state.cow })}</pre>
+    </Fragment>
+  );
+}
